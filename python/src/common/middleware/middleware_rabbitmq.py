254bc6db77a8bafaa0a0d72215a20f4f4ae06389
MicaelaAlonso <malonso@fi.uba.ar>
2026-04-04 12:15:55 -0300

    init Queue + start_consuming + stop_consumig
    Se crea la connection de Rabbit MQ. Luego se implementa la función de start_consuming con un callback de prueba (no hace nada todavía) y luego se implementa stop_consumig

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -1,12 +1,36 @@
 import pika
 import random
 import string
+import logging
 from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def __init__(self, host, queue_name):
+        self.queue_name = queue_name
+        self.connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=host))
+        self.channel = self.connection.channel()
+        response = self.channel.queue_declare(queue=self.queue_name, durable=True)
+        if response:
+            logging.debug(f"Queue declared successfully")
+    
+    def start_consuming(self, on_message_callback):
+        self.channel.basic_consume(queue=self.queue_name, on_message_callback=self.callback)
+        self.channel.start_consuming()
+    
+    
+    def callback(ch, method, properties, body):
+        logging.debug(f"Processing message: {body}")
+    
+    def stop_consuming(self):
+        self.channel.stop_consuming()
+
+    def send(self, message):
         pass
+
+    def close(self):
+        self.connection.close()
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
