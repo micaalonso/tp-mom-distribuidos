0fa8f085b5f9727ef8e2fa8c0212c77aceabce27
MicaelaAlonso <malonso@fi.uba.ar>
2026-04-07 19:48:16 -0300

    Refactor: reutilizo MessageMiddlewareQueueRabbitMQ para el exchange
    En vez de declarar la queue desde 0 en el exchange, reutilizo la clase de MessageMiddlewareQueueRabbitMQ. Para esto modifique un poco el init de MessageMiddlewareQueueRabbitMQ para que cree una queue con parametros que dependen de si el nombre de la queue es None (caso exchange) o no

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -1,25 +1,26 @@
 import pika
 import random
 import string
-import logging
 from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange, MessageMiddlewareMessageError, MessageMiddlewareDisconnectedError, MessageMiddlewareCloseError
 
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def __init__(self, host, queue_name):
-        self.queue_name = queue_name
         self.connection = pika.BlockingConnection(
         pika.ConnectionParameters(host=host))
         self.channel = self.connection.channel()
-        response = self.channel.queue_declare(queue=self.queue_name, durable=True)
+        if not queue_name:
+            # Sería para cuando armo el exchange, que le paso un string vacio como queue_name
+            response = self.channel.queue_declare(queue=queue_name, exclusive=True)
+        else:
+            response = self.channel.queue_declare(queue=queue_name, durable=True)
         if response:
-            logging.info(f"Queue declared successfully")
+            self.queue_name = response.method.queue
             print("Queue declared successfully")
     
     def start_consuming(self, on_message_callback):
         def callback(ch, method, properties, body):
             try:
-                logging.info(f"Processing message: {body}")
                 print(f"Processing message: {body}")
                 def ack():
                     ch.basic_ack(delivery_tag=method.delivery_tag)
@@ -71,33 +72,29 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     def __init__(self, host, exchange_name, routing_keys):
         self.exchange_name = exchange_name
         self.routing_keys = routing_keys
-        self.connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='rabbitmq'))
-        self.channel = self.connection.channel()
-        self.channel.exchange_declare(exchange=self.exchange_name, exchange_type='direct')
+        # self.connection = pika.BlockingConnection(
+        # pika.ConnectionParameters(host='rabbitmq'))
+        # self.channel = self.connection.channel()
+        # self.channel.exchange_declare(exchange=self.exchange_name, exchange_type='direct')
 
-        # El flag exclusive es para que una vez que la conexión cierre, la queue se elimine
-        queue_response = self.channel.queue_declare(queue='', durable=True, exclusive=True)
-        self.queue_name = queue_response.method.queue
+        # # El flag exclusive es para que una vez que la conexión cierre, la queue se elimine
+        # queue_response = self.channel.queue_declare(queue='', durable=True, exclusive=True)
+        # self.queue_name = queue_response.method.queue
+        self.queue = MessageMiddlewareQueueRabbitMQ(host, '')
+        self.queue_name = self.queue.queue_name
+        self.connection = self.queue.connection
+        self.channel = self.queue.channel
+
+        self.channel.exchange_declare(exchange=self.exchange_name, exchange_type='direct')
 
         for routing_key in self.routing_keys:
             self.channel.queue_bind(exchange=self.exchange_name, queue=self.queue_name, routing_key=routing_key)
 
     def start_consuming(self, on_message_callback):
-        def callback(ch, method, properties, body):
-            logging.info(f"Processing message: {body}")
-            print(f"Processing message: {body}")
-            def ack():
-                ch.basic_ack(delivery_tag=method.delivery_tag)
-            def nack():
-                ch.basic_nack(delivery_tag=method.delivery_tag)
-            on_message_callback(body, ack, nack)
-        
-        self.channel.basic_consume(queue=self.queue_name, on_message_callback=callback)
-        self.channel.start_consuming()
+        self.queue.start_consuming(on_message_callback=on_message_callback)
 
     def stop_consuming(self):
-        self.channel.stop_consuming()
+        self.queue.stop_consuming()
 
     def send(self, message):
         for rounting_key in self.routing_keys:
@@ -108,4 +105,4 @@ class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
             )
 
     def close(self):
-        self.connection.close()
+        self.queue.close()
